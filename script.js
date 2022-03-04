0edcf38c75bb7a350f4ddca3e1951e81eb0048ac
bkee9 <nancy.mwende@student.moringaschool.com>
2022-03-04 16:50:53 +0300

    modify LICENCE.md,index.html,script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+//select username input...select email input
+// var day = document.querySelector(".day");
+// var month = document.querySelector("input[name=month]");
+// console.log (month)
+//validate user input
+// const form = document.querySelector(".user-form");
+// form.addEventListener("submit",nancy);
+
+// //display the responses
+// function nancy (event){
+//     event.preventDefault()
+//     const day = document.querySelector(".day");
+//     const month = document.querySelector(".month");
+//     console.log( uday.value, month.value);
+
+// }
+//  let name = "";
+//  let inputForm = document.querySelector("#inputForm");
+//  let maleGender = [
+//    "Kwasi",
+//    "Kwadwo",
+//    "Kwabena",
+//    "Kwaku",
+//    "Yaw",
+//    "Kofi",
+//    "Kwame",
+//  ];
+//  let femaleGender = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
